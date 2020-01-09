a2ca3ef17f307560c8776c6f82dc6429dce0e464
Olivia Davis <olivia.davis593@gmail.com>
2020-01-08 20:03:50 -0600

    reconfigured files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import HelloWorld from './state-drills/HelloWorld'
+import Bomb from './state-drills/Bomb'
 import './App.css';
 
 function App() {
   return (
     <div>
+      <h2>Hello, world!</h2>
       <HelloWorld />
+      <h2>Bomb!</h2>
+      <Bomb />
     </div>
   );
 }
--- src/state-drills/HelloWorld.js
+++ src/state-drills/HelloWorld.js
@@ -10,13 +10,13 @@ class HelloWorld extends Component {
             <div className="HelloWorld">
                 <p>Hello, {this.state.who}!</p>
                 <button onClick={() => this.setState({who: 'world'})}>
-
+                    World
                 </button>
                 <button onClick={() => this.setState({who: 'friend'})}>
-
+                    Friend
                 </button>
                 <button onClick={() => this.setState({who: 'React'})}>
-
+                    React
                 </button>
             </div>
         )
